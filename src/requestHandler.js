b6709fc83ff1e244646f9de86cf2256e7d16cb25
thinkty <thinkty@naver.com>
2020-07-03 14:10:16 +0900

    added api request handler

--- src/requestHandler.js
+++ src/requestHandler.js
@@ -1,7 +1,16 @@
 'use strict';
 
+import fs from 'fs';
+
 const _default = function handleRequest(req, res) {
-  console.log(req);
+  try {
+    fs.writeFileSync('request.json', JSON.stringify(req));
+
+    return res.sendStatus(200);
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(500);
+  }
 };
 
 export { _default as default };

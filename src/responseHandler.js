b9d4aab1832b39af619247f7675079c0ee49e15d
thinkty <thinkty@naver.com>
2020-07-14 13:18:48 +0900

    handling context with intent name

--- src/responseHandler.js
+++ src/responseHandler.js
@@ -12,12 +12,11 @@ export default function generateResponse(body) {
   const userInput = queryResult.queryText;
   const messages = parseKeywords(queryResult.fulfillmentMessages);
   const action = queryResult.action; // Can be undefined if not provided
-  const outputContexts = queryResult.outputContexts;
 
   // Edit this part to send your own response
   return {
     fulfillmentMessages: createFulfillmentMessages(action, userInput, messages),
-    outputContexts: handleContextWithAction(action, userInput, outputContexts),
+    outputContexts: handleContext(queryResult),
     // followupEventInput: {
     //   name: "doesnotexist",
     //   languageCode: lang
@@ -29,6 +28,13 @@ export default function generateResponse(body) {
 const keywords = {
   name: "Bob"
 };
+
+// Sample intents
+const LOOP_FALLBACK = 1;
+const intentMap = {
+  "WakeUp-220": LOOP_FALLBACK
+};
+
 
 /**
  * Parse the keywords (words wrapped in brackets {}) and replace them
@@ -69,8 +75,30 @@ function parseKeywords(messages) {
 
 
 /**
- * If action is defined, form output context based on the input.
- * If action is not defined, pass on the given contexts.
+ * Handle the output contexts. If action is provided, it needs to make a
+ * decision based on the user input and send the appropriate output contexts.
+ * If action is not provided, it should make its decision based on the intent 
+ * name.
+ * 
+ * @param {Object} queryResult Fulfillment request from DialogFlow
+ */
+function handleContext(queryResult) {
+
+  const userInput = queryResult.queryText;
+  const action = queryResult.action;
+  const outputContexts = queryResult.outputContexts;
+  const intentName = queryResult.intent.displayName;
+
+  if (action) {
+    return handleContextWithAction(action, userInput, outputContexts);
+  }
+
+  // Action not provided
+  return handleContextWithIntentName(intentName, outputContexts);
+}
+
+/**
+ * If action is defined, form output context based on the user input.
  * 
  * @param {String}  action Defined by the intent
  * @param {String}   input User input
@@ -78,20 +106,35 @@ function parseKeywords(messages) {
  */
 function handleContextWithAction(action, input, contexts) {
 
-  if (!action) {
-    return contexts;
-  }
-
   if (action === 'checkAttendance') {
     if (input.length > 5) {
       return contexts;
     } else {
       return resetContext(contexts);
     }
-  } else {
-    return contexts;
+  }
+  
+  return contexts;
+}
+
+
+/**
+ * Form output context based on the intent name
+ * 
+ * @param {String} intentName Display name of the intent
+ * @param {Array}    contexts Default output contexts of the intent
+ */
+function handleContextWithIntentName(intentName, contexts) {
+
+  // If the given intent name is defined, handle it based on the map
+  if (intentMap[intentName]) {
+    if (intentMap[intentName] === LOOP_FALLBACK) {
+      return extendContext(contexts);
+    }
   }
 
+  // If the intent is not defined in the intent map, do not modify the contexts
+  return contexts;
 }
 
 
@@ -159,4 +202,28 @@ function resetContext(contexts) {
   });
 
   return resettedContexts;
+}
+
+
+/**
+ * Contrary to the function above, this function extends the given contexts'
+ * lifespanCount by 1.
+ * 
+ * @param {Array} contexts Contexts parsed from the query result
+ * @returns {Array} Output contexts with 0 lifespan count 
+ */
+function extendContext(contexts) {
+  if (!contexts || contexts.length === 0) {
+    return null;
+  }
+
+  let extendedContext = [];
+  contexts.forEach(context => {
+    extendedContext.push({
+      name: context.name,
+      lifespanCount: 1
+    });
+  });
+
+  return extendedContext;
 }
